249e5afbdd17476c3ed7b69c69d77ab9df529a30
pPaulaner690 <paula.krug2000@gmail.com>
2023-04-25 14:22:06 +0200

    add js-einführung

---
+++ 25.04.2023 JS_Einführung/Variablen/assets/js/main.js
@@ -0,0 +1,26 @@
+// let x = 34;
+// console.log(x);
+
+// x = 67;
+// console.log(x);
+
+// const y = 34;
+// y = 67;
+// console.log(y);
+
+let carName = "BMW";
+console.log(carName);
+
+let x = 150;
+console.log(x);
+
+let y = 50;
+console.log(y);
+
+let z = x+y;
+console.log(z);
+
+let firstName = "John";
+let lastName = "Doe";
+let age = 35;
+console.log(firstName, lastName, age);
---
+++ 25.04.2023 JS_Einführung/document.write/assets/js/main.js
@@ -0,0 +1,10 @@
+document.write("<h1>Hello World!</h1>");
+document.write("<p>Have a nice day!</p>");
+
+let myText = "Hello again";
+document.write(myText);
+
+let firstName = "Paula";
+let lastName = "Krug";
+
+document.write( "<p> Name: " + firstName + " <br> Nachname: " + lastName);
