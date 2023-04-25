02c32378141a2b9b80f9cf933ced606837803fe0
pPaulaner690 <paula.krug2000@gmail.com>
2023-04-25 14:58:21 +0200

    update document.write

--- 25.04.2023 JS_Einführung/document.write/assets/js/main.js
+++ 25.04.2023 JS_Einführung/document.write/assets/js/main.js
@@ -1,10 +1,11 @@
 document.write("<h1>Hello World!</h1>");
 document.write("<p>Have a nice day!</p>");
 
-let myText = "Hello again";
+let myText = "Hello again <br><br>";
 document.write(myText);
 
-let firstName = "Paula";
-let lastName = "Krug";
+let firstName = "Name: Paula <br>";
+let lastName = " Nachname: Krug";
 
-document.write( "<p> Name: " + firstName + " <br> Nachname: " + lastName);
+document.write(firstName);
+document.write(lastName);
