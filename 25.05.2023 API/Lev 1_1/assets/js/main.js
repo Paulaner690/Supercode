e2b6a783a245304c9d4e0cdf014e4df508624e7f
pPaulaner690 <paula.krug2000@gmail.com>
2023-05-25 12:02:48 +0200

    upload api

---
+++ 25.05.2023 API/Lev 1_1/assets/js/main.js
@@ -0,0 +1,8 @@
+fetch("https://picsum.photos/v2/list")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log("Fehler", error);
+  });
---
+++ 25.05.2023 API/Lev 2_1/assets/js/main.js
@@ -0,0 +1,32 @@
+// ! FETCH
+fetch("https://picsum.photos/v2/list")
+  .then((res) => res.json()) //   KONVERTIEREN IN JSON
+  .then((data) => {
+    // ! LOOP
+    data.forEach((element) => {
+      // KONSTANTEN FÜR DIE ELEMENTE IM FIGURE TAG:
+      const img = element.download_url;
+      const author = element.author;
+
+      // FIGURE TAG
+      const figureTag = document.createElement("figure");
+
+      // IMG IM FIGURE TAG
+      let imgOfTag = document.createElement("img");
+      imgOfTag.setAttribute("src", img);
+      imgOfTag.setAttribute("alt", "random Pic");
+      figureTag.appendChild(imgOfTag);
+
+      // AUTOR FIGCAPTION IM FIGURE TAG
+      let authorOfTag = document.createElement("figcaption");
+      authorOfTag.textContent = author;
+      figureTag.appendChild(authorOfTag);
+
+      //   ! AUSGABE IM HTML
+      document.querySelector("body").appendChild(figureTag);
+    });
+  })
+  //   ! CATCH, FALLS FETCH NICHT FUNKTIONIERT
+  .catch((err) => {
+    console.log("fehler", err);
+  });
